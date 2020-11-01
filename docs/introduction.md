8388579d4e823c0bf75d520d8d88b15a839cb16a
renatodex <renatodex@gmail.com>
2020-11-01 03:09:46 -0300

    Add social, and warning at introduction

--- docs/introduction.md
+++ docs/introduction.md
@@ -6,7 +6,9 @@ slug: /
 ---
 
 Olá, tudo bom?
-Primeiramente queremos te dar boas vindas a este novo universo. Tudo que você lerá aqui, foi cuidadosamente desenhado para te capacitar rapidamente a estar pronto para jogar uma partida de RPG de Mesa. 
+Primeiramente queremos te dar boas vindas a este novo universo. Tudo que você lerá aqui, foi cuidadosamente desenhado para te capacitar rapidamente a estar pronto para jogar uma partida de RPG de Mesa.
+
+O Fábulas & Goblins é um sistema de RPG totalmente independente e gratuito que está sendo mantido e desenvolvido pela <a href="https://www.instagram.com/tendadogoblin" nofollow>Tenda do Goblin</a>.
 
 O objetivo deste site é atuar como o livro digital do Fábulas & Goblins, te fornecendo uma introdução amigável e interativa do nosso conteúdo, e lhe capacitando para jogar uma partida de RPG de Mesa de forma fácil e rápida.
 Você também verá que ao longo do tempo estaremos fornecendo uma serie de ferramentas para auxiliar na construção dos personagens e no gerenciamento da sessão!
--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,73 @@ const features = [
   },
 ];
 
+const socials = [
+  {
+    title: 'Junte-se a nós no Instagram!',
+    imageUrl: 'img/socials/instagram.png',
+    description: (
+      <div>
+        Nossa página no Instagram está sempre trazendo novas atualizações do sistema e outras novidades.
+        É lá que postamos nossas últimas atualizações!
+      </div>
+    ),
+    cta_text: 'Seguir',
+    cta_link: 'https://www.instagram.com/tendadogoblin',
+  },
+  {
+    title: 'Canal do Discord',
+    imageUrl: 'img/socials/discord.png',
+    description: (
+      <div>
+        Criamos um canal especificamente para discussões sobre o sistema.
+        É lá que organizaremos mesas de teste e outros eventos!
+      </div>
+    ),
+    cta_text: 'Juntar-se ao Canal',
+    cta_link: 'https://discord.gg/PRMXj4j',
+  },
+  {
+    title: 'Colabore no Github',
+    imageUrl: 'img/socials/github.png',
+    description: (
+      <div>
+        Este site é um projeto de código aberto que pode ser evoluido colaborativamente
+        através da plataforma Github.
+        Quer ajudar a incrementar o projeto, ou traduzi-lo para outros idiomas?
+        O Github é o local perfeito!
+      </div>
+    ),
+    cta_text: 'Colaborar',
+    cta_link: 'https://github.com/renatodex/fabulas-e-goblins',
+  },
+];
+
 function Feature({imageUrl, title, description, cta_text, cta_link}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+
+      <Link
+        className={clsx(
+          'button button--outline button--success button--md',
+          styles.ctaButton,
+        )}
+        style={{marginBottom: '40px'}}
+        to={useBaseUrl(cta_link)}>
+        {cta_text}
+      </Link>
+    </div>
+  );
+}
+
+function Social({imageUrl, title, description, cta_text, cta_link}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={clsx('col col--4', styles.feature)}>
@@ -100,6 +166,22 @@ function Home() {
               <div className="row">
                 {features.map((props, idx) => (
                   <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+
+        <h1 style={{textAlign: 'center', width: '80%', margin: '0 auto'}}>
+          Fabulas & Goblins nas redes
+        </h1>
+
+        {socials && socials.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {socials.map((props, idx) => (
+                  <Social key={idx} {...props} />
                 ))}
               </div>
             </div>
