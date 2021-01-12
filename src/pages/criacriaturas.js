3eccb3cff2051891fd7cdc0a899c438828b678b8
renatodex <renatodex@gmail.com>
2021-01-12 18:13:58 -0300

    Add criacriaturas route

---
+++ src/pages/criacriaturas.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Redirect} from '@docusaurus/router';
+
+const Home = () => {
+  return <Redirect to="https://airtable.com/shrftjSexrVTNFcO3" />;
+};
+
+export default Home
