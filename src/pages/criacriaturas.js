ae35d37607815b97c800f174ba760cfb476e0990
renatodex <renatodex@gmail.com>
2021-01-12 18:16:35 -0300

    Change redirect

--- src/pages/criacriaturas.js
+++ src/pages/criacriaturas.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 const Home = () => {
-  return <Redirect to="https://airtable.com/shrftjSexrVTNFcO3" />;
+  window.location = "https://airtable.com/shrftjSexrVTNFcO3";
+  return null
 };
 
 export default Home
