4b8d26d88841c0e9d138c244d64cee4ba58b62ed
renatodex <renatodex@gmail.com>
2021-01-13 18:52:02 -0300

    Create your creature (en version of the form)

---
+++ src/pages/createyourcreature.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component {
+  componentDidMount () {
+    if (window) {
+      window.location = "https://airtable.com/shr59kf3ogC4NEFZy";
+    }
+  }
+
+  render () {
+    return null
+  }
+};
+
+export default Home
