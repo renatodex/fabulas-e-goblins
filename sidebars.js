d335fa63e14dd35694773a896a80f9484a51449d
Renato Alves <renatodex@gmail.com>
2021-01-29 20:39:33 -0300

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: [
     'introduction', {
-      'Começando a Jogar': ['1-starting-the-game/what-is'],
+      'Começando a Jogar': ['1-starting-the-game/what-is', '1-starting-the-game/players-role'],
     },
   ]
 };
