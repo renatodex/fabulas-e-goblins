60bae34f393356fd6abc261843652ff7777b28c3
renatodex <renatodex@gmail.com>
2021-02-16 19:37:18 -0300

    Add chapter 2 to sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,36 @@
 module.exports = {
   someSidebar: [
     'introduction', {
-      'Começando a Jogar': ['1-starting-the-game/what-is', '1-starting-the-game/players-role', '1-starting-the-game/gm-role', '1-starting-the-game/how-to-play'],
+      'Começando a Jogar': [
+        '1-starting-the-game/what-is',
+        '1-starting-the-game/players-role',
+        '1-starting-the-game/gm-role',
+        '1-starting-the-game/how-to-play'
+      ],
+      'Criando seu Personagem': [
+        '2-sheet-creation/getting-sheet',
+        '2-sheet-creation/pick-a-name',
+        '2-sheet-creation/pick-a-class',
+        '2-sheet-creation/money',
+        '2-sheet-creation/initial-level',
+        '2-sheet-creation/defining-your-culture',
+        '2-sheet-creation/defining-attributes',
+        '2-sheet-creation/calculating-modifiers',
+        '2-sheet-creation/movement-points',
+        '2-sheet-creation/armor_deduction',
+        '2-sheet-creation/hp-mp-defense',
+        '2-sheet-creation/hp-points',
+        '2-sheet-creation/mp-points',
+        '2-sheet-creation/magic-proficience',
+        '2-sheet-creation/defense-points',
+        '2-sheet-creation/attack-points',
+        '2-sheet-creation/initial-items',
+        '2-sheet-creation/grimos-and-magic',
+        '2-sheet-creation/acquiring-flaws',
+        '2-sheet-creation/add-flaws-in-the-game',
+        '2-sheet-creation/defining-your-origins',
+        '2-sheet-creation/deck-of-archetypes'
+      ]
     },
   ]
 };
