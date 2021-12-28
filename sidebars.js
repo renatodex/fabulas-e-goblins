ca87697c2fe004d56c60096b10db8106508f8f23
Renato Alves <renatodex@gmail.com>
2021-12-28 17:53:14 -0300

    Group Grimos used by Fastplay while keeping others in development

--- sidebars.js
+++ sidebars.js
@@ -41,15 +41,20 @@ module.exports = {
         '3-species/specie-luminins'
       ],
       '4. Escolhendo seu Grimo': [
+        '4-grimos-and-spells/grimos-introduction',
         '4-grimos-and-spells/giurads-crest',
         '4-grimos-and-spells/lunns-jewel',
         '4-grimos-and-spells/alluras-orb',
         '4-grimos-and-spells/eye-of-kanus',
-        '4-grimos-and-spells/ark-of-ravna',
-        '4-grimos-and-spells/darians-totem',
-        '4-grimos-and-spells/flask-of-valari',
-        '4-grimos-and-spells/magni-machine',
-        '4-grimos-and-spells/rune-of-ixin'
+        {
+          '[Em desenvolvimento]': [
+            '4-grimos-and-spells/ark-of-ravna',
+            '4-grimos-and-spells/darians-totem',
+            '4-grimos-and-spells/flask-of-valari',
+            '4-grimos-and-spells/magni-machine',
+            '4-grimos-and-spells/rune-of-ixin',
+          ]
+        }
       ],
       '5. Escolhendo um Papel de Jogo': [
         '5-roles/roles-introduction',
