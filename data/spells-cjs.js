34fdd749fcf8aca8156d7c8597c154b326b3e75c
Renato Alves <renatodex@gmail.com>
2022-01-04 02:35:16 -0300

    Add plugin to generate dynamic API

---
+++ data/spells-cjs.js
@@ -0,0 +1,27 @@
+const { resolve } = require('path');
+const { readdir } = require('fs').promises;
+
+async function getFiles(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(dirents.map((dirent) => {
+    const res = resolve(dir, dirent.name);
+    return dirent.isDirectory() ? getFiles(res) : res;
+  }));
+  return Array.prototype.concat(...files);
+}
+
+function getSpells() {
+  return new Promise((resolve, reject) => {
+    getFiles("data/").then((files) => {
+      let spells = files
+        .filter((path) => !path.includes('spells.js'))
+        .map((file) => require(file))
+
+      resolve(spells)
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
+
+module.exports = { getSpells }
