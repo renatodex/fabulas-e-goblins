e524ee5a73900797058404d4ba5e39039aec44f3
renatodex <renatodex@gmail.com>
2024-01-04 13:44:38 -0300

    Changes

--- src/components/equipment_block/index.jsx
+++ src/components/equipment_block/index.jsx
@@ -7,6 +7,7 @@ import { BsCoin } from "react-icons/bs";
 import { FaWeightHanging } from "react-icons/fa";
 import { GiWalkingBoot } from "react-icons/gi";
 import { GiFireAxe } from "react-icons/gi";
+import { BsBricks } from "react-icons/bs";
 
 export default function EquipmentBlock ({ data }) {
 
@@ -48,14 +49,14 @@ export default function EquipmentBlock ({ data }) {
 
   return (
     <div className="tailwindpage">
-      <div className="border border-orange-200 rounded shadow-md">
-        <div className="p-4 border-b border-orange-200 bg-orange-50">
-          <h3 className='text-lg font-bold'>
+      <div className="shadow-lg rounded-xl">
+        <div className="p-4 border-b border-orange-200 bg-sky-500 border-2 border-sky-800 text-white rounded-t-xl">
+          <h3 className='text-xl font-bold'>
             {name()} +{data.refinement}
           </h3>
         </div>
 
-        <div className="flex">
+        <div className="flex border-r-2 border-l-2 border-sky-800">
           <div className="flex-1">
             <div className="p-4 grid grid-cols-4 gap-3 text-2xl font-bold text-center">
               <div className="border p-2 border-gray-200 bg-gray-100 rounded-xl"><GiSwordBrandish className="inline"/> <span>1</span></div>
@@ -85,11 +86,11 @@ export default function EquipmentBlock ({ data }) {
           </div>
         </div>
 
-        <div className="p-4 border-t border-t-orange-200 grid grid-cols-4 bg-orange-50 gap-3 text-lg font-bold text-center">
-          <div className="border p-2 border-gray-200 bg-gray-100 rounded-xl"><BsCoin className="inline"/> <span>T$ {data.buy_price}</span></div>
-          <div className="border p-2 border-gray-200 bg-gray-100 rounded-xl"><FaWeightHanging className="inline"/> <span>{data.slots}</span></div>
-          <div className="border p-2 border-gray-200 bg-gray-100 rounded-xl"><GiWalkingBoot className="inline"/> <span>{data.speed}</span></div>
-          <div className="border p-2 border-gray-200 bg-gray-100 rounded-xl"><GiWalkingBoot className="inline"/> <span>{data.durability.current}/{data.durability.total}</span></div>
+        <div className="p-4 border-t border-t-orange-200 grid grid-cols-4 bg-sky-500 gap-3 text-lg font-bold text-center rounded-b-xl border-b-2 border-l-2 border-r-2 border-sky-800">
+          <div className="border p-2 bg-white text-black rounded-xl"><BsCoin className="inline"/> <span>T$ {data.buy_price}</span></div>
+          <div className="border p-2 bg-white text-black rounded-xl"><FaWeightHanging className="inline"/> <span>{data.slots}</span></div>
+          <div className="border p-2 bg-white text-black rounded-xl"><GiWalkingBoot className="inline"/> <span>{data.speed}</span></div>
+          <div className="border p-2 bg-white text-black rounded-xl"><BsBricks className="inline"/> <span>{data.durability.current}/{data.durability.total}</span></div>
         </div>
       </div>
     </div>
