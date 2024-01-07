e16bad288061960f759fbd188d845b557af9aacc
renatodex <renatodex@gmail.com>
2024-01-06 22:04:09 -0300

    Altera componente de equipamento para seguir design do Figma

--- src/components/equipment_block/index.jsx
+++ src/components/equipment_block/index.jsx
@@ -49,14 +49,64 @@ export default function EquipmentBlock ({ data }) {
 
   return (
     <div className="tailwindpage">
-      <div className="shadow-lg rounded-xl">
-        <div className="p-4 border-b border-orange-200 bg-sky-500 border-2 border-sky-800 text-white rounded-t-xl">
+      <div className="shadow-lg rounded-xl bg-indigo-500 p-5">
+        <div className="bg-indigo-500 text-white rounded-t-xl">
           <h3 className='text-xl font-bold'>
             {name()} +{data.refinement}
           </h3>
         </div>
 
-        <div className="flex border-r-2 border-l-2 border-sky-800">
+        <div className='bg-indigo-500 mt-3'>
+          <div className="grid grid-cols-2 gap-3 p-3 bg-white rounded-xl">
+            <div className='grid sm:block gap-3 sm:gap-0'>
+              <div className='sm:flex'>
+                <div className='border flex-1 border-b-0 sm:border-r-0 border-black px-2 py-1 rounded-t-xl sm:rounded-r-none'>Dano Total</div>
+                <div className='border flex-1 sm:border-b-0 border-black px-2 py-1 sm:rounded-t-xl rounded-b-xl sm:rounded-br-none sm:rounded-l-none'>d12 + 4</div>
+              </div>
+              <div className='sm:flex'>
+                <div className='border flex-1 border-b-0 sm:border-r-0 border-black rounded-t-xl sm:rounded-t-none px-2 py-1'>Ataque (F/M)</div>
+                <div className='border flex-1 sm:border-b-0 border-black rounded-b-xl sm:rounded-b-none px-2 py-1'>d12 + 4</div>
+              </div>
+              <div className='sm:flex'>
+                <div className='border flex-1 border-b-0 sm:border-r-0 border-black rounded-t-xl sm:rounded-t-none px-2 py-1'>Defesa (F/M)</div>
+                <div className='border flex-1 sm:border-b-0 border-black rounded-b-xl sm:rounded-b-none px-2 py-1'>d12 + 4</div>
+              </div>
+              <div className='sm:flex'>
+                <div className='border flex-1 border-black sm:border-r-0 px-2 py-1 rounded-t-xl sm:rounded-t-none sm:rounded-br-none sm:rounded-b-xl sm:rounded-r-none'>Movimento</div>
+                <div className='border flex-1 border-black px-2 py-1 sm:rounded-b-xl rounded-b-xl sm:rounded-b-none sm:rounded-bl-none sm:rounded-l-none'>-1</div>
+              </div>
+            </div>
+
+            <div className="grid sm:block gap-3 sm:gap-0">
+              <div className='sm:flex'>
+                <div className='border flex-1 border-b-0 sm:border-r-0 border-black px-2 py-1 rounded-t-xl sm:rounded-r-none sm:flex'>
+                  <div className='flex-1'>Grau:</div>
+                  <div>
+                    <span className='rounded-full mx-0.5 border-black border inline-block w-5 text-sm text-center font-bold bg-purple-400'>1</span>
+                    <span className='rounded-full mx-0.5 border-black border inline-block w-5 text-sm text-center font-bold bg-white'>2</span>
+                    <span className='rounded-full mx-0.5 border-black border inline-block w-5 text-sm text-center font-bold bg-white'>3</span>
+                    <span className='rounded-full mx-0.5 border-black border inline-block w-5 text-sm text-center font-bold bg-white'>4</span>
+                  </div>
+                </div>
+                <div className='border flex-1 sm:border-b-0 border-black px-2 py-1 sm:rounded-t-xl rounded-b-xl sm:rounded-br-none sm:rounded-l-none bg-gray-200'>Ocupa:</div>
+              </div>
+              <div className='sm:flex'>
+                <div className='border flex-1 border-b-0 sm:border-r-0 border-black rounded-t-xl sm:rounded-t-none px-2 py-1'>Durabilidade:</div>
+                <div className='border flex-1 sm:border-b-0 border-black rounded-b-xl sm:rounded-b-none px-2 py-1'></div>
+              </div>
+              <div className='sm:flex'>
+                <div className='border flex-1 border-b-0 sm:border-r-0 border-black rounded-t-xl sm:rounded-t-none px-2 py-1'>Attributo</div>
+                <div className='border flex-1 sm:border-b-0 border-black rounded-b-xl sm:rounded-b-none px-2 py-1'>Sobrevivência</div>
+              </div>
+              <div className='sm:flex'>
+                <div className='border flex-1 border-black sm:border-r-0 px-2 py-1 rounded-t-xl sm:rounded-t-none sm:rounded-br-none sm:rounded-b-xl sm:rounded-r-none'>Preço de Venda</div>
+                <div className='border flex-1 border-black px-2 py-1 sm:rounded-b-xl rounded-b-xl sm:rounded-b-none sm:rounded-bl-none sm:rounded-l-none'>T$ 20.000</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="flex border-r-2 border-l-2 border-indigo-800">
           <div className="flex-1">
             <div className="p-4 grid grid-cols-4 gap-3 text-2xl font-bold text-center">
               <div className="border p-2 border-gray-200 bg-gray-100 rounded-xl"><GiSwordBrandish className="inline"/> <span>1</span></div>
@@ -84,14 +134,14 @@ export default function EquipmentBlock ({ data }) {
               })}
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className="p-4 border-t border-t-orange-200 grid grid-cols-4 bg-sky-500 gap-3 text-lg font-bold text-center rounded-b-xl border-b-2 border-l-2 border-r-2 border-sky-800">
+        {/* <div className="p-4 border-t border-t-orange-200 grid grid-cols-4 bg-indigo-500 gap-3 text-lg font-bold text-center rounded-b-xl border-b-2 border-l-2 border-r-2 border-indigo-800">
           <div className="border p-2 bg-white text-black rounded-xl"><BsCoin className="inline"/> <span>T$ {data.buy_price}</span></div>
           <div className="border p-2 bg-white text-black rounded-xl"><FaWeightHanging className="inline"/> <span>{data.slots}</span></div>
           <div className="border p-2 bg-white text-black rounded-xl"><GiWalkingBoot className="inline"/> <span>{data.speed}</span></div>
           <div className="border p-2 bg-white text-black rounded-xl"><BsBricks className="inline"/> <span>{data.durability.current}/{data.durability.total}</span></div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
