9d515f45fa557dfd2afd4703da626724f9bcb10e
renatodex@gmail.com <renatodex@gmail.com>
2024-05-23 03:36:56 -0300

    Add Tier property

--- data/spells/spells-cjs.js
+++ data/spells/spells-cjs.js
@@ -10,23 +10,29 @@ async function getFiles(dir) {
   return Array.prototype.concat(...files);
 }
 
+function getTierFromPath(path) {
+  const match = path.match(/tier(\d+)/);
+  return match ? parseInt(match[1], 10) : null;
+}
+
 function getSpells() {
   return new Promise((resolve, reject) => {
     getFiles("data/spells").then((files) => {
       let spells = files
         .filter((path) => !path.includes('spells.js'))
         .map((file) => {
-          const fileData = require(file)
-          const parts = file.replace(/\\/g, "/").split(".").filter(part => part != "json")[0].split("/")
-          const fileName = parts[parts.length-1]
-          return { id: fileName, ...fileData }
-        })
+          const fileData = require(file);
+          const parts = file.replace(/\\/g, "/").split(".").filter(part => part !== "json")[0].split("/");
+          const fileName = parts[parts.length - 1];
+          const tier = getTierFromPath(file);
+          return { id: fileName, tier, ...fileData };
+        });
 
-      resolve(spells)
+      resolve(spells);
     }).catch(err => {
-      reject(err)
-    })
-  })
+      reject(err);
+    });
+  });
 }
 
-module.exports = { getSpells }
+module.exports = { getSpells };
